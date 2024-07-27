059548bcb34ff3a3e6b7d0648b3ba3b0b3066431
Luke Yamaguchi <yamaluke@g.ucla.edu>
2024-07-27 15:50:42 -0700

    set up motion based keyboard use effects. Still have not connected to socket yet.

--- HAcK web/ccweb/src/App.js
+++ HAcK web/ccweb/src/App.js
@@ -2,11 +2,51 @@ import './App.css';
 import React, { useState, useEffect, useRef } from "react";
 import io from 'socket.io-client';
 
-// const socket = io('http://localhost:8000');
+const socket = io('http://localhost:8000');
 
 function App() {
 
+  const baseColor = 'white';
 
+  const [textColorW, setTextColorW] = useState(baseColor);
+  const [textColorS, setTextColorS] = useState(baseColor);
+  const [textColorA, setTextColorA] = useState(baseColor);
+  const [textColorD, setTextColorD] = useState(baseColor);
+
+  useEffect( () => {
+    const handleKeyDown = (event) => {
+      if(event.key === 'w'){
+        setTextColorW('blue');
+      }else if (event.key === 's'){
+        setTextColorS('blue');
+      }else if (event.key === 'a'){
+        setTextColorA('blue');
+      }else if (event.key === 'd'){
+        setTextColorD('blue');
+      }
+    }
+
+    const handleKeyUp = (event) => {
+      if(event.key === 'w'){
+        setTextColorW(baseColor);
+      }else if (event.key === 's'){
+        setTextColorS(baseColor);
+      }else if (event.key === 'a'){
+        setTextColorA(baseColor);
+      }else if (event.key === 'd'){
+        setTextColorD(baseColor);
+      }
+      
+    }
+
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyUp);
+      window.removeEventListener('keyup', handleKeyUp);
+    }
+  })
 
 
   return (
@@ -15,6 +55,12 @@ function App() {
         <h1 className="title"> 
           Control Center
         </h1>
+        <div id = 'buttonLayout'>
+          <h3 className="keyboardInputShow" style = {{color: textColorW}}> Foward (W) </h3>
+          <h3 className="keyboardInputShow" style = {{color: textColorS}}> Backwards (S) </h3>
+          <h3 className="keyboardInputShow" style = {{color: textColorA}}> Left (A) </h3>
+          <h3 className="keyboardInputShow" style = {{color: textColorD}}> Right (D) </h3>
+        </div>
 
       </div>
     </div>
