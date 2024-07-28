2e85100305bb7c309bfa7706b7a79a93513ec6c5
Luke Yamaguchi <yamaluke@g.ucla.edu>
2024-07-27 19:58:39 -0700

    basic website structure created

--- HAcK web/ccweb/src/App.js
+++ HAcK web/ccweb/src/App.js
@@ -6,35 +6,68 @@ const socket = io('http://localhost:8000');
 
 function App() {
 
+  // base color for text that uses useState
   const baseColor = 'white';
 
+  // useState for the direction: 
+    // will change colors on screen when a key is pressed
   const [textColorW, setTextColorW] = useState(baseColor);
   const [textColorS, setTextColorS] = useState(baseColor);
   const [textColorA, setTextColorA] = useState(baseColor);
   const [textColorD, setTextColorD] = useState(baseColor);
 
+  // useState for the sensor variables 
+  const [temp, setTemp] = useState('-0')
+  const [ultrasonic, setUltrasonic] = useState('-0');
+  const [humidity, setHumidity] = useState('-0');
+
   useEffect( () => {
+    // keyboard handle for directional buttons being pressed
     const handleKeyDown = (event) => {
       if(event.key === 'w'){
         setTextColorW('blue');
+        socket.emit('send-direction', 'fowardsGo')
       }else if (event.key === 's'){
         setTextColorS('blue');
+        socket.emit('send-direction', 'backwardsGo')
       }else if (event.key === 'a'){
         setTextColorA('blue');
+        socket.emit('send-direction', 'leftGo')
       }else if (event.key === 'd'){
         setTextColorD('blue');
+        socket.emit('send-direction', 'rightGo')
       }
     }
 
+    // Listen for temperature updates
+    socket.on('temp', (data) => {
+      setTemp(data);
+    });
+
+    // Listen for ultrasonic updates
+    socket.on('ultrasonic', (data) => {
+      setUltrasonic(data);
+    });
+
+    // listen for humidity sensor
+    socket.on('humidity', (data) => {
+      setHumidity(data);
+    });
+
+    // turn keys back to normal
     const handleKeyUp = (event) => {
       if(event.key === 'w'){
         setTextColorW(baseColor);
+        socket.emit('send-direction', 'fowardsStop')
       }else if (event.key === 's'){
         setTextColorS(baseColor);
+        socket.emit('send-direction', 'backwardsStop')
       }else if (event.key === 'a'){
         setTextColorA(baseColor);
+        socket.emit('send-direction', 'leftStop')
       }else if (event.key === 'd'){
         setTextColorD(baseColor);
+        socket.emit('send-direction', 'rightStop')
       }
       
     }
@@ -45,6 +78,9 @@ function App() {
     return () => {
       window.removeEventListener('keydown', handleKeyUp);
       window.removeEventListener('keyup', handleKeyUp);
+      socket.off('temp');
+      socket.off('ultrasonic');
+      socket.off('humidity')
     }
   })
 
@@ -60,6 +96,11 @@ function App() {
           <h3 className="keyboardInputShow" style = {{color: textColorS}}> Backwards (S) </h3>
           <h3 className="keyboardInputShow" style = {{color: textColorA}}> Left (A) </h3>
           <h3 className="keyboardInputShow" style = {{color: textColorD}}> Right (D) </h3>
+        </div>
+        <div id = 'sensorLayout'>
+          <h3 className="sensorData"> Temperature: {temp}</h3>
+          <h3 className='sensorData'> Ultrasonic distance: {ultrasonic}</h3>
+          <h3 className='sensorData'> Humidity: {humidity}</h3>
         </div>
 
       </div>
